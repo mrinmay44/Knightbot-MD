21a818a30e4d1a16c6adb64780a74f9d8321af93
mrinmay44 <riayr8455@gmail.com>
2025-02-25 06:59:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'mrinmay',
   author: 'Bot',
-  botName: "knight Bot",
+  botName: "mrinmay bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '929345605793', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  version: "3.4.5",
 };
 
 module.exports = settings;
